e3f1e7318d3867d6ff3a1fcce8d38120c62c934d
yaboduul <80867970+yaboduul@users.noreply.github.com>
2022-04-15 17:10:23 -0500

    app.py
    app.py allows for not having to load flask app on every start

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\2953n\Documents\GitHub\PortfolioSite\venv
+set VIRTUAL_ENV=C:\Users\2953n\OneDrive\Documents\GitHub\PortfolioSite\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
